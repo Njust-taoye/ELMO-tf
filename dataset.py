7f161b6c12cbb4c79aa35c2ff8c5619a292030f7
codertimo <codertimo@gmail.com>
2018-12-03 17:20:57 +0900

    Fix predicting current word to next word

--- dataset.py
+++ dataset.py
@@ -18,21 +18,26 @@ class ElmoKoreanDataset:
 
     def text_to_char_sequence(self, text):
         jamo_text = self.jamo_processor.str_to_jamo(text)
-        char_idx_seq, seq_len = self.char_vocab.to_seq(jamo_text, char_seq_len=self.char_seq_len, seq_len=self.seq_len,
+        char_idx_seq, seq_len = self.char_vocab.to_seq(jamo_text,
+                                                       char_seq_len=self.char_seq_len,
+                                                       seq_len=self.seq_len,
                                                        with_len=True)
         seq_len = self.seq_len if seq_len > self.seq_len else seq_len
         return char_idx_seq, seq_len
 
     def text_to_word_sequence(self, text):
-        word_idx_seq, seq_len = self.word_vocab.to_seq(text, seq_len=self.seq_len, with_len=True)
-        seq_len = self.seq_len if seq_len > self.seq_len else seq_len
+        word_idx_seq, seq_len = self.word_vocab.to_seq(text, seq_len=self.seq_len+1, with_len=True)
+        seq_len = self.seq_len + 1 if seq_len > self.seq_len + 1 else seq_len
+        word_idx_seq, seq_len = word_idx_seq[1:], seq_len - 1
         return word_idx_seq, seq_len
 
     def produce_data(self, text):
         text = text.strip()
         char_word_input, input_len = self.text_to_char_sequence(text)
         word_target, target_len = self.text_to_word_sequence(text)
-        return {"input": char_word_input, "input_len": input_len, "target": word_target, "target_len": target_len}
+
+        return {"input": char_word_input, "input_len": input_len,
+                "target": word_target, "target_len": target_len}
 
     def data_generator(self):
         for file_path in self.corpus_files:
